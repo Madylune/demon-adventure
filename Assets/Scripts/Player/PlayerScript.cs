6956ac54543a767a2e14b297868dfec3da88c1ab
Julie Nginn <nginn.julie@gmail.com>
2020-11-11 16:10:04 +0100

    Feat: Add player's health bar + username

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+    [SerializeField]
+    private string username;
+
+    [SerializeField]
+    private float currentHealth, maxHealth;
+
+    [SerializeField]
+    private HealthBar healthBar;
+
+    [SerializeField]
+    private Text usernameText;
+
     private PlayerMovement movement;
     private PlayerAttack attack;
 
@@ -28,5 +41,55 @@ public class PlayerScript : MonoBehaviour
     {
         MyMovement = GetComponent<PlayerMovement>();
         MyAttack = GetComponent<PlayerAttack>();
+
+        currentHealth = maxHealth;
+        healthBar.Initialize(currentHealth, maxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(10);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Heal(100);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if ((currentHealth - damage) < 0)
+        {
+            currentHealth = 0;
+            //Die()
+        }
+        else
+        {
+            currentHealth -= damage;
+            CombatTextManager.MyInstance.CreateText(transform.position, damage.ToString(), CombatTextType.DAMAGE, false);
+        }
+        healthBar.SetHealth(currentHealth);
+    }
+
+    public void Heal(int amount)
+    {
+        if ((currentHealth + amount) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+
+        healthBar.SetHealth(currentHealth);
+        CombatTextManager.MyInstance.CreateText(transform.position, amount.ToString(), CombatTextType.HEAL, false);
+    }
+
+    public void Die()
+    {
+
     }
 }
