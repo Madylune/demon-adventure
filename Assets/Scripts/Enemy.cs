e134c969537b7408a8675c8e2e4511dbd4e2bcb6
Julie Nginn <nginn.julie@gmail.com>
2020-11-11 15:32:43 +0100

    Feat: Add enemy's title

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,13 +9,17 @@ public class Enemy : MonoBehaviour
     private string title;
 
     [SerializeField]
-    private float minAtk, maxAtk, currentHealth, maxHealth = 1000;
+    private float level, minAtk, maxAtk, currentHealth, maxHealth = 1000;
 
     [SerializeField]
     private HealthBar healthBar;
 
+    [SerializeField]
+    private Text titleText;
+
     void Start()
     {
+        titleText.text = title + " (" + level + ")";
         currentHealth = maxHealth;
         healthBar.Initialize(currentHealth, maxHealth);
     }
