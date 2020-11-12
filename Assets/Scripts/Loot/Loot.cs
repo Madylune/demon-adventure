1adf47967443e75e36f881c4e188923c207f037e
Julie Nginn <nginn.julie@gmail.com>
2020-11-12 18:44:10 +0100

    Loot: open loot window

---
+++ Assets/Scripts/Loot/Loot.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Loot : MonoBehaviour
+{
+    [SerializeField] private float range;
+    private bool isClickable = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (DetectPlayer())
+            {
+                LootManager.MyInstance.PopWindow();
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private bool DetectPlayer()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, range);
+        if (hit != null && hit.tag == "Player")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/Loot/LootManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    private static LootManager instance;
+
+    public static LootManager MyInstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<LootManager>();
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField] private GameObject lootWindow;
+    [SerializeField] private Transform mainCanvas;
+
+    public void PopWindow()
+    {
+        Instantiate(lootWindow, mainCanvas);
+    }
+}
