95228d7b1b8fa4f93572e8ffbaded53a67eddfeb
Julie Nginn <nginn.julie@gmail.com>
2020-11-13 15:45:11 +0100

    Loot: Store items in inventory

--- Assets/Scripts/Loot/LootButton.cs
+++ Assets/Scripts/Loot/LootButton.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class LootButton : MonoBehaviour
+public class LootButton : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private Image icon;
     [SerializeField] private Text title;
 
     public Image MyIcon { get => icon; }
     public Text MyTitle { get => title; }
+    public Item MyLoot { get; set; }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        PlayerScript.MyInstance.MyInventory.AddItem(MyLoot);
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/Loot/UI_Loot.cs
+++ Assets/Scripts/Loot/UI_Loot.cs
@@ -7,18 +7,34 @@ public class UI_Loot : MonoBehaviour
     [SerializeField] private LootButton lootButtonPrefab;
     [SerializeField] private Transform lootsContainer;
 
+    float MAX_LOOTS = 4;
+
     private void Start()
     {
         AddLoot(new Item { itemType = Item.ItemType.Arrow, amount = 1 });
         AddLoot(new Item { itemType = Item.ItemType.Coin, amount = 10 });
     }
 
+    private void Update()
+    {
+        RefreshLoots();
+    }
+
     private void AddLoot(Item item)
     {
         LootButton lootButton = Instantiate(lootButtonPrefab, lootsContainer);
+        lootButton.MyLoot = item;
 
         lootButton.MyIcon.sprite = item.GetSprite();
         lootButton.MyTitle.text = item.itemType.ToString() + " x" + item.amount.ToString();
+    }
+
+    public void RefreshLoots()
+    {
+        if (lootsContainer.childCount <= 0)
+        {
+            CloseWindow();
+        }
     }
 
     public void CloseWindow()
