b6265e182ccef58292f1de5dbaf46cb165fb32ac
Julie Nginn <nginn.julie@gmail.com>
2020-11-13 15:33:02 +0100

    Loot: Get loots when window is open

---
+++ Assets/Scripts/Loot/LootButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LootButton : MonoBehaviour
+{
+    [SerializeField] private Image icon;
+    [SerializeField] private Text title;
+
+    public Image MyIcon { get => icon; }
+    public Text MyTitle { get => title; }
+}
--- Assets/Scripts/Loot/UI_Loot.cs
+++ Assets/Scripts/Loot/UI_Loot.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class UI_Loot : MonoBehaviour
 {
+    [SerializeField] private LootButton lootButtonPrefab;
+    [SerializeField] private Transform lootsContainer;
+
+    private void Start()
+    {
+        AddLoot(new Item { itemType = Item.ItemType.Arrow, amount = 1 });
+        AddLoot(new Item { itemType = Item.ItemType.Coin, amount = 10 });
+    }
+
+    private void AddLoot(Item item)
+    {
+        LootButton lootButton = Instantiate(lootButtonPrefab, lootsContainer);
+
+        lootButton.MyIcon.sprite = item.GetSprite();
+        lootButton.MyTitle.text = item.itemType.ToString() + " x" + item.amount.ToString();
+    }
+
     public void CloseWindow()
     {
         Destroy(gameObject);
